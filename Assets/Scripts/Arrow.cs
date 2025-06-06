485e72b39c4c305ee65bebe0ad3d831359619f50
sondmfpt <sondmhe186291@fpt.edu.vn>
2025-06-06 15:56:33 +0700

    update archer attack

---
+++ Assets/Scripts/Arrow.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+    public float maxDistance = 10f;
+    private Vector3 startPos;
+
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, startPos) > maxDistance)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
