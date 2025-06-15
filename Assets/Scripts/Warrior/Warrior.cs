e0c475c9f6cbfacf18889f3c718695b96e50daf3
DucNghiem <147522506+DucNghiem@users.noreply.github.com>
2025-06-15 23:11:15 +0700

    animation for warrior weapon

--- Assets/Scripts/Warrior/Warrior.cs
+++ Assets/Scripts/Warrior/Warrior.cs
@@ -1,39 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Warrior : CharacterCommonBehavior
 {
     public override float moveSpeed { get; set; } = 4f;
-    public GameObject axePrefabs;
-    public Transform throwPoint;
+    private Animator animator;
+    public GameObject axe;
+    public Transform axeSpawnPoint;
+    public float timeBtwaxe = 0.2f;
+    public float bulletForce;
+    public float currentHp;
 
-    public float throwForce = 10f;
     // Start is called before the first frame update
 
     // Update is called once per frame
+    private float _timeBtwaxe = 0.2f;
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
     protected override void Update()
     {
         base.Update();
-        if (Input.GetMouseButtonDown(0))
+        _timeBtwaxe -= Time.deltaTime;
+        if (Input.GetMouseButtonDown(0) && _timeBtwaxe < 0)
         {
+            animator.SetTrigger("isAttack");
             Attack();
         }
     }
     public override void Attack()
     {
-        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector2 direction = (mousePosition - throwPoint.position).normalized;
+        _timeBtwaxe = timeBtwaxe;
 
-        GameObject axe = Instantiate(axePrefabs, throwPoint.position, Quaternion.identity);
-        Rigidbody2D rb = axe.GetComponent<Rigidbody2D>();
-        if (rb != null)
-        {
-            rb.velocity = direction * throwForce;
-        }
+        // Lấy vị trí chuột trên màn hình và chuyển sang tọa độ thế giới
+        Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mouseWorldPos.z = 0f;
+        // Tính hướng từ điểm bắn đến vị trí chuột
+        Vector2 direction = (mouseWorldPos - axeSpawnPoint.position).normalized;
 
-        // Xoay rìu theo hướng bay (tùy hiệu ứng)
+        // Tạo tên
+        GameObject newaxe = Instantiate(axe, axeSpawnPoint.position, Quaternion.identity);
+
+        // Xoay tên theo hướng bắn (nếu muốn)
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-        axe.transform.rotation = Quaternion.Euler(0, 0, angle);
+        newaxe.transform.rotation = Quaternion.Euler(0f, 0f, angle);
+            
+             // Thêm lực
+            Rigidbody2D arrowRb = newaxe.GetComponent<Rigidbody2D>();
+            arrowRb.AddForce(direction * bulletForce, ForceMode2D.Impulse);
     }
 }
