1c45ade99bd444c699b4f493250949c5106a316d
Lawlez57 <trungkilomet2@gmail.com>
2025-06-21 18:58:02 +0700

    Update Abysal Skill

---
+++ Assets/Scripts/Common/Weapons/Abysal/AbysalImpact.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbysalImpact : MonoBehaviour
+{
+    public float destroyDelay = 0.5f;
+    private Animator animator;
+    private bool hasExploded = false;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (hasExploded) return;
+
+        if (other.CompareTag("Enemy"))
+        {
+            hasExploded = true;
+
+            animator.SetTrigger("Impact");
+
+            GetComponent<Collider2D>().enabled = false;
+            GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+
+            if (other.CompareTag("Enemy"))
+            {
+                other.GetComponent<Enemy>()?.TakeDamage(10);
+            }
+
+            Destroy(gameObject, destroyDelay);
+        }
+    }
+}
