f30972102d23506ec9faaba961b1646483a00640
DucNghiem <147522506+DucNghiem@users.noreply.github.com>
2025-06-26 15:55:14 +0700

    fix axe

---
+++ Assets/Scripts/Warrior/Skill1_Warrior.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skill1_Warrior : SkillBase
+{
+    protected override void Activate()
+    {
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
