0e4020f8318310cf66e80edde0f228bed2b00317
sondmfpt <sondmhe186291@fpt.edu.vn>
2025-07-01 14:54:47 +0700

    add sound for char attack

--- Assets/Scripts/CharacterCommonBehavior.cs
+++ Assets/Scripts/CharacterCommonBehavior.cs
@@ -19,6 +19,7 @@ public abstract class CharacterCommonBehavior : MonoBehaviour
     public CommonUI commonUI;
     private float onMovingCharacterHorizontal;
     private const string ENERMY_WEAPON = "Enemy_Weapon";
+    public AudioClip attackSound;
 
     private bool isInvincible = false;
     private float invincibleEndTime = 0f;
@@ -36,7 +37,8 @@ public abstract class CharacterCommonBehavior : MonoBehaviour
     public static event Action OnBlockedCollision;
 
     //audio
-    private AudioManager audioManager;
+    [HideInInspector]
+    public AudioManager audioManager;
 
 
     private void Awake()
