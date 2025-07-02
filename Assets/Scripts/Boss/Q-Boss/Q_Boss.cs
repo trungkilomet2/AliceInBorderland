1942796875ff6d03d4e010df4e77c3ce4727b24e
AnhPQHE180725 <anhpqhe180725@fpt.edu.vn>
2025-07-02 21:58:15 +0700

    add q boss

---
+++ Assets/Scripts/Boss/Q-Boss/Q_Boss.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Q_Boss : BossBase
+{
+    protected override void Awake()
+    {
+        base.Awake();
+        SetTarget(GameObject.FindGameObjectWithTag("Player"));
+    }
+    void Update()
+    {
+        if (targetCharacter == null)
+        {
+            return;
+        }
+        // Set điều kiện để sử dụng kỹ năng
+        BossSkillBase skill1 = bossSkillBases[0];
+        if (Vector3.Distance(transform.position, targetCharacter.transform.position) < skill1.skillRange) 
+        {
+            skill1.UseSkill();
+        }
+        BossSkillBase skill2 = bossSkillBases[1];
+        if (Vector3.Distance(transform.position, targetCharacter.transform.position) < skill2.skillRange)
+        {
+            skill2.UseSkill();
+        }
+
+    }
+}
